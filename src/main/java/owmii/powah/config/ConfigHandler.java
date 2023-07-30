28be9ea97e9834d22ff41351dae2f938b75912bb
Gcat101 <gcatmail2@gmail.com>
2023-07-30 14:38:02 +0300

    fix the bug Tazz was complaining about

--- src/main/java/owmii/powah/config/ConfigHandler.java
+++ src/main/java/owmii/powah/config/ConfigHandler.java
@@ -33,27 +33,25 @@ public class ConfigHandler {
                 String regName = args[0];
                 if (regName.contains(":")) {
                     ResourceLocation rl = new ResourceLocation(regName);
-                    if (ModList.get().isLoaded(rl.getNamespace().toLowerCase())) {
-                        try {
-                            Fluid fluid = ForgeRegistries.FLUIDS.getValue(rl);
-                            if (fluid != null) {
-                                try {
-                                    int coldness = Integer.parseInt(args[1]);
-                                    if (coldness < 2 && coldness > -101) {
-                                        COOLANT_FLUIDS.put(fluid, coldness);
-                                        LOGGER.info(MARKER, "Added coolant fluid: {}, with coldness of: {} per mb", regName, args[1]);
-                                    } else
-                                        LOGGER.warn(MARKER, "Ignored coolant fluid: {}, with coldness > 1 or < -100: {}", regName, args[1]);
-                                } catch (Exception e) {
-                                    LOGGER.fatal(MARKER, "Ignored coolant fluid: {}, for incorrect args: {}", regName, args[1]);
-                                    e.printStackTrace();
-                                }
-                            } else LOGGER.fatal(MARKER, "Ignored Wrong coolant fluid registry name {}.", regName);
-                        } catch (Exception e) {
-                            LOGGER.fatal(MARKER, "Ignored Wrong coolant fluid registry name {}.", regName);
-                            e.printStackTrace();
-                        }
-                    } else LOGGER.warn(MARKER, "Ignored coolant fluid for missing mod: {}.", rl.getNamespace());
+                    try {
+                        Fluid fluid = ForgeRegistries.FLUIDS.getValue(rl);
+                        if (fluid != null) {
+                            try {
+                                int coldness = Integer.parseInt(args[1]);
+                                if (coldness < 2 && coldness > -101) {
+                                    COOLANT_FLUIDS.put(fluid, coldness);
+                                    LOGGER.info(MARKER, "Added coolant fluid: {}, with coldness of: {} per mb", regName, args[1]);
+                                } else
+                                    LOGGER.warn(MARKER, "Ignored coolant fluid: {}, with coldness > 1 or < -100: {}", regName, args[1]);
+                            } catch (Exception e) {
+                                LOGGER.fatal(MARKER, "Ignored coolant fluid: {}, for incorrect args: {}", regName, args[1]);
+                                e.printStackTrace();
+                            }
+                        } else LOGGER.fatal(MARKER, "Ignored Wrong coolant fluid registry name {}.", regName);
+                    } catch (Exception e) {
+                        LOGGER.fatal(MARKER, "Ignored Wrong coolant fluid registry name {}.", regName);
+                        e.printStackTrace();
+                    }
                 } else LOGGER.fatal(MARKER, "Ignored Wrong coolant fluid registry name {}.", regName);
             }
         });
@@ -69,27 +67,25 @@ public class ConfigHandler {
                 String regName = args[0];
                 if (regName.contains(":")) {
                     ResourceLocation rl = new ResourceLocation(regName);
-                    if (ModList.get().isLoaded(rl.getNamespace().toLowerCase())) {
-                        try {
-                            Block block = ForgeRegistries.BLOCKS.getValue(rl);
-                            if (block != null && block != Blocks.AIR && ForgeRegistries.BLOCKS.containsKey(rl)) {
-                                try {
-                                    int heat = Integer.parseInt(args[1]);
-                                    if (heat > 0 && heat < 900000000) {
-                                        HEAT_SOURCES.put(block, heat);
-                                        LOGGER.info(MARKER, "Added block: {}, with heat of: {}", regName, args[1]);
-                                    } else
-                                        LOGGER.warn(MARKER, "Ignored block: {}, with heat < 1 or > 900000000: {}", regName, args[1]);
-                                } catch (Exception e) {
-                                    LOGGER.fatal(MARKER, "Ignored block: {}, for incorrect args: {}", regName, args[1]);
-                                    e.printStackTrace();
-                                }
-                            } else LOGGER.fatal(MARKER, "Ignored air or unregistered block registry name {}.", regName);
-                        } catch (Exception e) {
-                            LOGGER.fatal(MARKER, "Ignored Wrong block registry name {}.", regName);
-                            e.printStackTrace();
-                        }
-                    } else LOGGER.warn(MARKER, "Ignored block for missing mod: {}.", rl.getNamespace());
+                    try {
+                        Block block = ForgeRegistries.BLOCKS.getValue(rl);
+                        if (block != null && block != Blocks.AIR && ForgeRegistries.BLOCKS.containsKey(rl)) {
+                            try {
+                                int heat = Integer.parseInt(args[1]);
+                                if (heat > 0 && heat < 900000000) {
+                                    HEAT_SOURCES.put(block, heat);
+                                    LOGGER.info(MARKER, "Added block: {}, with heat of: {}", regName, args[1]);
+                                } else
+                                    LOGGER.warn(MARKER, "Ignored block: {}, with heat < 1 or > 900000000: {}", regName, args[1]);
+                            } catch (Exception e) {
+                                LOGGER.fatal(MARKER, "Ignored block: {}, for incorrect args: {}", regName, args[1]);
+                                e.printStackTrace();
+                            }
+                        } else LOGGER.fatal(MARKER, "Ignored air or unregistered block registry name {}.", regName);
+                    } catch (Exception e) {
+                        LOGGER.fatal(MARKER, "Ignored Wrong block registry name {}.", regName);
+                        e.printStackTrace();
+                    }
                 } else LOGGER.fatal(MARKER, "Ignored Wrong block registry name {}.", regName);
             }
         });
@@ -105,27 +101,25 @@ public class ConfigHandler {
                 String regName = args[0];
                 if (regName.contains(":")) {
                     ResourceLocation rl = new ResourceLocation(regName);
-                    if (ModList.get().isLoaded(rl.getNamespace().toLowerCase())) {
-                        try {
-                            Fluid fluid = ForgeRegistries.FLUIDS.getValue(rl);
-                            if (fluid != null) {
-                                try {
-                                    int heat = Integer.parseInt(args[1]);
-                                    if (heat > 0 && heat <= 900000000) {
-                                        MAGMATIC_FLUIDS.put(fluid, heat);
-                                        LOGGER.info(MARKER, "Added fluid: {}, with heat of: {} per 100 mb", regName, args[1]);
-                                    } else
-                                        LOGGER.warn(MARKER, "Ignored fluid: {}, with heat < 1 or > 900000000: {}", regName, args[1]);
-                                } catch (Exception e) {
-                                    LOGGER.fatal(MARKER, "Ignored fluid: {}, for incorrect args: {}", regName, args[1]);
-                                    e.printStackTrace();
-                                }
-                            } else LOGGER.fatal(MARKER, "Ignored Wrong fluid registry name {}.", regName);
-                        } catch (Exception e) {
-                            LOGGER.fatal(MARKER, "Ignored Wrong fluid registry name {}.", regName);
-                            e.printStackTrace();
-                        }
-                    } else LOGGER.warn(MARKER, "Ignored fluid for missing mod: {}.", rl.getNamespace());
+                    try {
+                        Fluid fluid = ForgeRegistries.FLUIDS.getValue(rl);
+                        if (fluid != null) {
+                            try {
+                                int heat = Integer.parseInt(args[1]);
+                                if (heat > 0 && heat <= 900000000) {
+                                    MAGMATIC_FLUIDS.put(fluid, heat);
+                                    LOGGER.info(MARKER, "Added fluid: {}, with heat of: {} per 100 mb", regName, args[1]);
+                                } else
+                                    LOGGER.warn(MARKER, "Ignored fluid: {}, with heat < 1 or > 900000000: {}", regName, args[1]);
+                            } catch (Exception e) {
+                                LOGGER.fatal(MARKER, "Ignored fluid: {}, for incorrect args: {}", regName, args[1]);
+                                e.printStackTrace();
+                            }
+                        } else LOGGER.fatal(MARKER, "Ignored Wrong fluid registry name {}.", regName);
+                    } catch (Exception e) {
+                        LOGGER.fatal(MARKER, "Ignored Wrong fluid registry name {}.", regName);
+                        e.printStackTrace();
+                    }
                 } else LOGGER.fatal(MARKER, "Ignored Wrong fluid registry name {}.", regName);
             }
         });
