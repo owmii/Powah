0e1859b576fd99953e4cc6861742a5bea044c1e3
rainbuu <aaronemoryd@gmail.com>
2023-11-20 18:54:07 -0500

    wip stuff i want slep

--- src/main/java/owmii/powah/armor/ArmorEffectsHandler.java
+++ src/main/java/owmii/powah/armor/ArmorEffectsHandler.java
@@ -1,7 +1,7 @@
 package owmii.powah.armor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,7 +11,6 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -25,14 +24,15 @@ import owmii.powah.Powah;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = Powah.MOD_ID)
 public class ArmorEffectsHandler {
-    private static HashMap<String, ArmorEffect> handlers = new HashMap<>();
+    private static LinkedHashMap<String, ArmorEffect> handlers = new LinkedHashMap<>();
     static {
         handlers.put("starter", new ArmorEffect(player -> {player.getPersistentData().putBoolean("isThunderImmune", true);}, player -> {player.getPersistentData().putBoolean("isThunderImmune", false);}));
         handlers.put("basic", new ArmorEffect());
+        handlers.put("hardened", new ArmorEffect());
         handlers.put("blazing", new ArmorEffect());
         handlers.put("spirited", new ArmorEffect());
         handlers.put("niotic", new ArmorEffect());
-        handlers.put("nitro", new ArmorEffect());
+        handlers.put("nitro", new ArmorEffect(player -> {player.getPersistentData().putBoolean("hasHealthBoost", true);}, player -> {player.getPersistentData().putBoolean("hasHealthBoost", false);}));
         handlers.put("overcharged", new ArmorEffect(player -> {player.abilities.allowFlying = true;}, player -> {player.abilities.allowFlying = false; player.abilities.isFlying = false;}));
 
     }
@@ -44,7 +44,7 @@ public class ArmorEffectsHandler {
 
         ArrayList<String> tiers = new ArrayList<>(handlers.keySet());
         List<ArmorEffect> effects = handlers.values().stream().collect(Collectors.toList());
-
+        System.out.println(tiers);
         Item fromItem = e.getFrom().getItem();
         String itemId = fromItem.getRegistryName().toString();
         if (Pattern.compile(Powah.MOD_ID+":energized_(helmet|chestplate|leggings|boots)_\\w+").matcher(itemId).matches()) {
@@ -95,8 +95,8 @@ public class ArmorEffectsHandler {
             return;
         }
 
-        e.getToolTip().add(new StringTextComponent("[SHIFT]"));
-        e.getToolTip().add(new StringTextComponent(""));
+        e.getToolTip().add(new StringTextComponent("[SHIFT]").mergeStyle(TextFormatting.GREEN));
+        e.getToolTip().add(new StringTextComponent("----------------------").mergeStyle(TextFormatting.GRAY));
         for (int i = 0; i <= tiers.indexOf(tier); i++) {
             String langKey = "armor_effect."+Powah.MOD_ID+"."+tiers.get(i);
             if (I18n.hasKey(langKey)) e.getToolTip().add(new StringTextComponent("- ").append(new TranslationTextComponent(langKey)).mergeStyle(TextFormatting.GRAY));
--- src/main/java/owmii/powah/armor/ArmorPotions.java
+++ src/main/java/owmii/powah/armor/ArmorPotions.java
@@ -5,13 +5,15 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import owmii.powah.Powah;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = Powah.MOD_ID)
-public class ThunderImmunity {
+public class ArmorPotions {
     @SubscribeEvent
     public static void onPlayerDamage(LivingHurtEvent e) {
         if (!(e.getEntity() instanceof PlayerEntity)) return;
@@ -22,6 +24,15 @@ public class ThunderImmunity {
         if (e.getSource()==DamageSource.LIGHTNING_BOLT && nbt.contains("isThunderImmune") && nbt.getBoolean("isThunderImmune")) {
             player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 60, 1, false, false));
             e.setCanceled(true);
+        }
+    }
+    @SubscribeEvent
+    public static void onTick(TickEvent.PlayerTickEvent e) {
+        PlayerEntity player = e.player;
+        CompoundNBT nbt = player.getPersistentData();
+
+        if (nbt.contains("hasHealthBoost") && nbt.getBoolean("hasHealthBoost")) {
+            player.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST, 40, 4, false, false));
         }
     }
 }
